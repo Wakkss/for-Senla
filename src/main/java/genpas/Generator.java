27a1dfa6dcbc5f9286d18706abee70c3a942511c
sevas <seva.salnikov.2003@yandex.ru>
2024-10-25 15:45:52 +0300

    Tasks

--- src/main/java/genpas/Generator.java
+++ src/main/java/genpas/Generator.java
@@ -30,7 +30,7 @@ public class Generator {
         return res;
     }
 
-    //рандомная цифры
+    //рандомная цифра
     public static char[] num(char[] res, int l) {
         char[] numbers = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
         int x = ranIndex(res, l);
